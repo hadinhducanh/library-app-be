7ec6ade9619b4a845070a8dcf0b487af80d7eccd
hadinhducanh <ducanhhadinh@gmail.com>
2024-04-11 09:48:53 +0700

    library-be

---
+++ spring-boot-library/src/main/java/com/ducanh/springbootlibrary/controller/ReviewController.java
@@ -0,0 +1,39 @@
+package com.ducanh.springbootlibrary.controller;
+
+import com.ducanh.springbootlibrary.requestmodels.ReviewRequest;
+import com.ducanh.springbootlibrary.service.ReviewService;
+import com.ducanh.springbootlibrary.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping("/secure/user/book")
+    public Boolean reviewBookByUser(@RequestHeader(value="Authorization") String token,
+                                    @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        return reviewService.userReviewListed(userEmail, bookId);
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value="Authorization") String token,
+                           @RequestBody ReviewRequest reviewRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
